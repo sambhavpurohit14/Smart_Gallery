fd1c9d325ac3067970895bcbed28d73506b6ab9e
HarshitR2004 <harshithranjan6971@gmail.com>
2025-03-15 13:39:59 +0530

    Update db_routes.py

--- smart_gallery_backend/routes/db_routes.py
+++ smart_gallery_backend/routes/db_routes.py
@@ -13,7 +13,6 @@ async def initialize_db(request: InitializeDBRequest):
     try:
         user_id = str(uuid.uuid4())
         db_manager = ImageDBManager(user_id)
-        user_db = db_manager.collection
         return {"message": f"Initialized DB for user {request.user_name} with ID {user_id}"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
