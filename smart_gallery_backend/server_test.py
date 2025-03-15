c9e3fc496889d2c52a63be7fb10309958e945607
HarshitR2004 <harshithranjan6971@gmail.com>
2025-03-16 01:18:49 +0530

    updated test

--- smart_gallery_backend/server_test.py
+++ smart_gallery_backend/server_test.py
@@ -1,13 +1,8 @@
 import chromadb
 
-chroma_client = chromadb.PersistentClient(path="/mnt/chromadb")
-
-# Create a collection
-collection = chroma_client.get_or_create_collection(name="my_collection")
-
-collection.add(
-    ids=["test1"],
-    documents=["Hello, ChromaDB from persistent disk!"]
+chroma_client = chromadb.HttpClient(
+    host="34.58.248.218",
+    port=8000
 )
+chroma_client.heartbeat()
 
-print("ChromaDB is set up with persistence!")
